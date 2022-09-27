00590673d36dd6b21fc688d602552e103b7cc0a3
Luke <lukeweston1234@gmail.com>
2022-09-27 00:50:26 -0400

    auth changes

--- routes/login.js
+++ routes/login.js
@@ -6,22 +6,24 @@ const googleAuth = require("../middlware/googleAuth");
 
 router.post("/", googleAuth, async (req, res) => {
   try {
+    const email = req.body.email;
     const query = `
     SELECT * FROM users 
     WHERE email = $1
     `;
-    const { rows } = await db.query(query, [req.email]);
+    const { rows } = await db.query(query, [email]);
     if (rows) {
       return res.status(200).json(rows[0]);
     }
   } catch (error) {
     try {
+      const email = req.body.email;
       const query = `
       INSERT INTO users (email)
       VALUES ($1)
       RETURNING *
       `;
-      const { rows } = await db.query(query, [req.email]);
+      const { rows } = await db.query(query, email);
       console.log(rows);
       if (rows) {
         return res.status(201).send("User Added");
