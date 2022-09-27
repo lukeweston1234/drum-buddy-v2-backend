b35ef3bd426e76aa655866af5e459c9860c25c3e
Luke <lukeweston1234@gmail.com>
2022-09-27 01:25:38 -0400

    auth flow

--- routes/login.js
+++ routes/login.js
@@ -12,28 +12,26 @@ router.post("/", googleAuth, async (req, res) => {
     WHERE email = $1
     `;
     const { rows } = await db.query(query, [email]);
+    console.log("Rows One", rows);
     if (rows[0]) {
       return res.status(200).json(rows[0]);
-    }
-  } catch (error) {
-    try {
-      const email = req.email;
-      const query = `
+    } else {
+      const query2 = `
       INSERT INTO users (email)
       VALUES ($1)
       RETURNING *
       `;
-      const { rows } = await db.query(query, email);
-      console.log(rows);
-      if (rows[0]) {
-        return res.status(201).send("User Added");
+      const { rows2 } = await db.query(query2, [email]);
+      console.log("Rows2", rows2);
+      if (rows2[0]) {
+        return res.status(201).json(rows[0]);
       } else {
         return res.status(500).send("Could not add User");
       }
-    } catch (error) {
-      console.log(error);
-      return res.status(403).send("Unauthorized");
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Could not add or find User");
   }
 });
 
